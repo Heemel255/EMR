cf5bd390c34006ad849c9149232b13eee38c2152
Heemel255 <heemel255@gmail.com>
2021-10-03 19:10:45 -0400

    v3.12

--- src/core/Application.java
+++ src/core/Application.java
@@ -12,7 +12,6 @@ import model.Helper.MySQLConn;
 public class Application
 {
 	public static void main(String[] args) {
-		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
